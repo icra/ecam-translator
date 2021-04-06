958567a5b7c69eb991debaae2519a3dbe350db76
rbrugues <roserbrugues@gmail.com>
2021-04-06 13:37:37 +0200

    checkbox for langs to translate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,82 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+    <!-- CHECKBOX TO SELECT LANGUAGES -->
+    <p>{{ langs_selected }}</p>
+    <v-row align-content="center">
+      <v-col cols="auto" align-self="center">
+        <strong>Select the language(s) you want to translate:</strong>
       </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
+      <v-col cols="auto">
+        <v-checkbox
+            v-model="langs_selected"
+            label="Arabic"
+            value="ar"
+        ></v-checkbox>
       </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
+      <v-col cols="auto">
+        <v-checkbox
+            v-model="langs_selected"
+            label="French"
+            value="fr"
+        ></v-checkbox>
       </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
+      <v-col cols="auto">
+        <v-checkbox
+            v-model="langs_selected"
+            label="German"
+            value="de"
+        ></v-checkbox>
       </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+      <v-col cols="auto">
+        <v-checkbox
+            v-model="langs_selected"
+            label="Spanish"
+            value="es"
+        ></v-checkbox>
+      </v-col>
+      <v-col cols="auto">
+        <v-checkbox
+            v-model="langs_selected"
+            label="Thai"
+            value="th"
+        ></v-checkbox>
       </v-col>
     </v-row>
+
+    <!-- TABLE WITH TAGS TO TRANSLATE -->
+    <div>
+      <v-data-table
+          :headers="headers"
+          :items="desserts"
+          item-key="name"
+          class="elevation-1"
+          :search="search"
+          :custom-filter="filterOnlyCapsText"
+      >
+        <template v-slot:top>
+          <v-text-field
+              v-model="search"
+              label="Search (UPPER CASE ONLY)"
+              class="mx-4"
+          ></v-text-field>
+        </template>
+        <template v-slot:body.append>
+          <tr>
+            <td></td>
+            <td>
+              <v-text-field
+                  v-model="calories"
+                  type="number"
+                  label="Less than"
+              ></v-text-field>
+            </td>
+            <td colspan="4"></td>
+          </tr>
+        </template>
+      </v-data-table>
+    </div>
   </v-container>
+
 </template>
 
 <script>
@@ -96,56 +84,41 @@
     name: 'HelloWorld',
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      langs_selected: [],
     }),
+    computed: {
+      headers () {
+        // fer que consulti els langs_selected per afegir el header de l'idioma seleccionat.
+        return [
+          {
+            text: 'ENGLISH',
+            align: 'start',
+            sortable: false,
+            value: 'en',
+          },
+          {
+            text: 'Calories',
+            value: 'calories',
+            filter: value => {
+              if (!this.calories) return true
+
+              return value < parseInt(this.calories)
+            },
+          },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ]
+      },
+    },
+    methods: {
+      filterOnlyCapsText (value, search, item) {
+        return value != null &&
+            search != null &&
+            typeof value === 'string' &&
+            value.toString().toLocaleUpperCase().indexOf(search) !== -1
+      },
+    },
   }
 </script>
